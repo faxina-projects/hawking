af8f9633d794e214ca40e37320ccdd0871a83269
Tiago Henrique Faxina <thfaxina@gmail.com>
2022-03-11 00:16:38 -0300

    add app config types

--- api/src/main/config/app.config.ts
+++ api/src/main/config/app.config.ts
@@ -1,4 +1,9 @@
-export const appConfig: any = {
+type AppConfig = {
+  name: string;
+  port: number;
+};
+
+export const appConfig: AppConfig = {
   name: 'hawking',
   port: Number(process.env.SERVER_PORT || 3000),
 };
