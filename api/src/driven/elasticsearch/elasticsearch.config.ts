91b6c68aecf14f0af5e3ce1037bd6e7a50821516
Tiago Henrique Faxina <thfaxina@gmail.com>
2022-03-10 21:52:08 -0300

    add elastic config type

--- api/src/driven/elasticsearch/elasticsearch.config.ts
+++ api/src/driven/elasticsearch/elasticsearch.config.ts
@@ -1,6 +1,13 @@
-export const elasticsearchConfig: any = {
-  host: process.env.ELASTICSEARCH_HOST,
+type ElasticsearchConfig = {
+  host: string;
+  port: number;
+  username: string;
+  password: string;
+};
+
+export const elasticsearchConfig: ElasticsearchConfig = {
+  host: process.env.ELASTICSEARCH_HOST as string,
   port: Number(process.env.ELASTICSEARCH_PORT),
-  username: process.env.ELASTICSEARCH_USERNAME,
-  password: process.env.ELASTICSEARCH_PASSWORD,
+  username: process.env.ELASTICSEARCH_USERNAME as string,
+  password: process.env.ELASTICSEARCH_PASSWORD as string,
 };
